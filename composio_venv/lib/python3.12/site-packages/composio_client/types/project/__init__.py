a84809a390e74ee5d133cd319c2258b86f2e1929
Ben Jammin <ben@xbenjamminx.com>
2026-02-03 16:09:41 +0000

    Remove composio_venv from version control (already in gitignore)

--- composio_venv/lib/python3.12/site-packages/composio_client/types/project/__init__.py
+++
@@ -1,7 +0,0 @@
-# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
-
-from __future__ import annotations
-
-from .config_update_params import ConfigUpdateParams as ConfigUpdateParams
-from .config_update_response import ConfigUpdateResponse as ConfigUpdateResponse
-from .config_retrieve_response import ConfigRetrieveResponse as ConfigRetrieveResponse
--- composio_venv/lib/python3.12/site-packages/composio_openai/provider.py
+++
@@ -1,8 +0,0 @@
-"""
-OpenAI Provider for composio SDK.
-"""
-
-from composio.core.provider._openai import OpenAIProvider
-from composio.core.provider._openai_responses import OpenAIResponsesProvider
-
-__all__ = ["OpenAIProvider", "OpenAIResponsesProvider"]
